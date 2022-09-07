2c343cb9ebaa2d4e04553385685cd1641ff9715d
Андрей Кулишов <andryxakulishow@gmail.com>
2022-09-07 21:30:57 +0300

    First task

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// first task
+const userInput = Number(prompt('Введите любое число: '));
+let result;
+
+// second task
+result = 18 + userInput;
+
+// third task
+result -= 5;
+result *= 5;
+result /= 5;
+
+// 4 task - No, it's const
+
+// 5 task
+alert(`This is the result - ${result}`);
+alert(`This is user input - ${userInput}`);
